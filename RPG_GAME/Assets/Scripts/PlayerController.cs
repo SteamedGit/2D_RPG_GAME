0e10adca82a1b5c42f7437a9642a49e0c33beecd
Tim <timothyhitge@gmail.com>
2019-09-05 22:26:44 +0200

    Basic Movement Set-Up
    Run, Idle and Jump are working.
    Collision detection still needs some work.

---
+++ RPG_GAME/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRenderer;
+
+    bool isGrounded;
+
+    [SerializeField]
+    Transform groundCheck;
+
+    public Transform GroundCheck { get => groundCheck; set => groundCheck = value; }
+
+    [SerializeField]
+    private float runSpeed = 1.5f;
+
+    [SerializeField]
+    private float jumpSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>(); 
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false; 
+        }
+
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(runSpeed, rb2d.velocity.y);
+            if (isGrounded)
+            {
+                animator.Play("Player_run");
+            }
+           
+            spriteRenderer.flipX = false;
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-runSpeed, rb2d.velocity.y);
+            if (isGrounded)
+            {
+                animator.Play("Player_run");
+            } 
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            if (isGrounded)
+            {
+                animator.Play("Player_idle");
+            }
+           
+        }
+
+        if (Input.GetKey("space") && isGrounded)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+            animator.Play("Player_jump");
+        }
+    }
+}
