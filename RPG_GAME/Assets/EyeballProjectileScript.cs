191ab32f2b1a9c4321b0c6ec9db94563ea547bd3
Tim <timothyhitge@gmail.com>
2020-07-29 02:12:21 +0200

    Eyeball Projectile Animation added in.

---
+++ RPG_GAME/Assets/EyeballProjectileScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeballProjectileScript : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    int damage;
+    
+    public void StartShoot()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+}
